ee9548ed9f2198d0bb53dd386617f456fc61b36a
ariagoose <73117176+ariagoose@users.noreply.github.com>
2020-12-08 14:09:21 -0500

    added endpoints controller

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PagesController {
 
-    @GetMapping("/")
+   /* @GetMapping("/")
     public String hello(){
         return "Hello from Spring!";
-    }
+    }*/
 
 }
