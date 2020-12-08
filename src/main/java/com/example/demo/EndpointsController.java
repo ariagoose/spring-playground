7167b66716c22881ae2af400abea710a6c3c2d5f
ariagoose <73117176+ariagoose@users.noreply.github.com>
2020-12-08 15:31:18 -0500

    Step 1 for queries complete

--- src/main/java/com/example/demo/EndpointsController.java
+++ src/main/java/com/example/demo/EndpointsController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,30 @@ public class EndpointsController {
     @GetMapping("/math/pi")
     public String getPi(){
         return "3.141592653589793";
+    }
+
+    @GetMapping("/math/calculate")
+    public String getCalc(@RequestParam String operation, @RequestParam int x, @RequestParam int y){
+        String result;
+        int answer;
+        String operand;
+
+        if (operation.equals("divide")){
+            operand = " / ";
+            answer = x/y;
+        }else if (operation.equals("subtract")){
+            operand = " - ";
+            answer = x-y;
+        }else if (operation.equals("multiply")){
+            operand = " * ";
+            answer = x*y;
+        }else {
+            operand = " + ";
+            answer = x+y;
+        }
+
+        result = x + operand + y + " = " + answer;
+        return result;
     }
 
     @GetMapping("/")
--- src/test/java/com/example/demo/EndpointsControllerTest.java
+++ src/test/java/com/example/demo/EndpointsControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(EndpointsController.class)
 public class EndpointsControllerTest {
@@ -25,4 +26,39 @@ public class EndpointsControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("3.141592653589793"));
     }
+
+    @Test
+    public void testCalc() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 5 = 6"));
+    }
+
 }
