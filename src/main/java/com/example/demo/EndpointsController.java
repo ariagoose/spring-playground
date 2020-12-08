face505997aa30bd3a94a5adbe84491b870548ff
ariagoose <73117176+ariagoose@users.noreply.github.com>
2020-12-08 14:19:30 -0500

    Added math/pi route and tested successfully

--- src/main/java/com/example/demo/EndpointsController.java
+++ src/main/java/com/example/demo/EndpointsController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EndpointsController {
 
+    @GetMapping("/math/pi")
+    public String getPi(){
+        return "3.141592653589793";
+    }
+
     @GetMapping("/")
     public String getIndex(){
         return "GET to index route";
---
+++ src/test/java/com/example/demo/EndpointsControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EndpointsController.class)
+public class EndpointsControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testPi() throws Exception{
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/pi");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("3.141592653589793"));
+    }
+}
